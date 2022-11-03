00cce0273ce02b3e1f7a8edd2751a9ca99f7e94a
AnaBelén <anhecblock1990@gmail.com>
2022-11-03 15:16:41 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import strictEquals from './module.js';
 
-console.log('Expect True', strictEquals(1, 1));
-console.log('Expect false', strictEquals(NaN, NaN));
-console.log('Expect True', strictEquals(0, -0));
-console.log('Expect True', strictEquals(-0, 0));
-console.log('Expect false', strictEquals(1, '1'));
-console.log('Expect false', strictEquals(true, false));
-console.log('Expect true', strictEquals(false, false));
-console.log('Expect false', strictEquals('Water', 'oil'));
+console.log('(1,1)-Expect True ', strictEquals(1, 1));
+console.log('(NaN, NaN)-Expect false', strictEquals(NaN, NaN));
+console.log('(0, -0)-Expect True', strictEquals(0, -0));
+console.log('(-0, 0)-Expect True', strictEquals(-0, 0));
+console.log('(1, "1")-Expect false', strictEquals(1, '1'));
+console.log('(true, false)-Expect false', strictEquals(true, false));
+console.log('(false, false)-Expect true', strictEquals(false, false));
+console.log('("water", "oil")-Expect false', strictEquals('water', 'oil'));
